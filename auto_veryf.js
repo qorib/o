355ce8a89d0e7fdd7d20d711f7f6f8314c8dc370
Amin <amin4udin@gmail.com>
2019-04-06 22:35:44 +0700

    update auto_veryf.js

--- auto_veryf.js
+++ auto_veryf.js
@@ -134,18 +134,121 @@ const functionGetLocation = domain =>
       .split(" ");
 
     const regEX = /[.-\w]+@[\w\-]{3,}(.\w{2,})+/;
-    const test = array.map(ury => {
-      return ury.match(regEX);
-    });
+    // const test = await array.map(ury => {
+    //   return ury.match(regEX);
+    // });
 
-    test.map(async eml => {
-      if (eml !== null) {
+    array.map(async eml => {
+      if (eml.length > 2) {
         const regMail = /(?<=@)[^.]+.([^.]+)$/m;
 
-        const uname = eml[0].split("@")[0];
-        const domain = eml[0].match(regMail)[0];
+        const uname = eml.substring(0, eml.lastIndexOf("@"));
+        const domain = eml.substring(eml.lastIndexOf("@") + 1);
 
         const email = uname + "@" + domain;
+
+        if (uname.includes("generator")) {
+          const unem = uname.split("/")[3];
+          await delay(DelaY);
+          const message = await functionGetMessages(unem, domain);
+
+          if (message === undefined) {
+            console.log(
+              "[" +
+                " " +
+                moment().format("HH:mm:ss") +
+                " " +
+                "]" +
+                " " +
+                "TOKEN EXPIRED / BELUM ADA EMAIL....."
+            );
+          } else {
+            if (message < 60) {
+              const getLocation = await functionGetLocation(message);
+
+              const regex = await new RegExp(/\?(?:code)\=([\S\s]*?)\&/);
+              const regexEm = await new RegExp(/[.\w]+@[\w\-]{3,}(.\w{2,})+/);
+              const resGex = await regex.exec(getLocation);
+              const resGexEm = await regexEm.exec(getLocation);
+
+              await delay(DelaY);
+              const veryf = await functionVerification(resGexEm[0], resGex[1]);
+              console.log(veryf);
+              // const msg = JSON.parse(veryf).error.status;
+
+              // if (JSON.parse(veryf).hasOwnProperty("error")) {
+              //   console.log(
+              //     "[" +
+              //       " " +
+              //       moment().format("HH:mm:ss") +
+              //       " " +
+              //       "]" +
+              //       " " +
+              //       `Email : ${resGexEm[0]}` +
+              //       " " +
+              //       "Token Expired"
+              //   );
+              // } else {
+              //   console.log(
+              //     "[" +
+              //       " " +
+              //       moment().format("HH:mm:ss") +
+              //       " " +
+              //       "]" +
+              //       " " +
+              //       `Email : ${resGexEm[0]}` +
+              //       " " +
+              //       "Veryf Sukses"
+              //   );
+              // }
+            } else {
+              const decodeURL = await decodeURIComponent(message);
+
+              const regex = await new RegExp(/\?(?:code)\=([\S\s]*?)\&/);
+              const regexEm = await new RegExp(
+                /([\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/
+              );
+              const resGex = await regex.exec(decodeURL);
+              const resGexEm = await regexEm.exec(decodeURL);
+
+              if (resGexEm !== null) {
+                await delay(DelaY);
+                const veryf = await functionVerification(
+                  resGexEm[0],
+                  resGex[1]
+                );
+                // const msg = JSON.parse(veryf).error.status;
+                console.log(veryf);
+
+                // if (JSON.parse(veryf).hasOwnProperty("error")) {
+                //   console.log(
+                //     "[" +
+                //       " " +
+                //       moment().format("HH:mm:ss") +
+                //       " " +
+                //       "]" +
+                //       " " +
+                //       `Email : ${resGexEm[0]}` +
+                //       " " +
+                //       "Token Expired"
+                //   );
+                // } else {
+                //   console.log(
+                //     "[" +
+                //       " " +
+                //       moment().format("HH:mm:ss") +
+                //       " " +
+                //       "]" +
+                //       " " +
+                //       `Email : ${resGexEm[0]}` +
+                //       " " +
+                //       "Veryf Sukses"
+                //   );
+                // }
+              }
+            }
+          }
+        }
 
         await delay(DelaY);
         const message = await functionGetMessages(uname, domain);
@@ -160,16 +263,15 @@ const functionGetLocation = domain =>
               " " +
               "TOKEN EXPIRED / BELUM ADA EMAIL....."
           );
-          console.log("");
-          console.log("");
         } else {
-          if (message == 44) {
+          if (message < 60) {
             const getLocation = await functionGetLocation(message);
 
             const regex = await new RegExp(/\?(?:code)\=([\S\s]*?)\&/);
             const regexEm = await new RegExp(/[.\w]+@[\w\-]{3,}(.\w{2,})+/);
             const resGex = await regex.exec(getLocation);
             const resGexEm = await regexEm.exec(getLocation);
+
             await delay(DelaY);
             const veryf = await functionVerification(resGexEm[0], resGex[1]);
             // const msg = JSON.parse(veryf).error.status;
@@ -209,47 +311,40 @@ const functionGetLocation = domain =>
             const resGex = await regex.exec(decodeURL);
             const resGexEm = await regexEm.exec(decodeURL);
 
-            await delay(DelaY);
-            const veryf = await functionVerification(resGexEm[0], resGex[1]);
-            // const msg = JSON.parse(veryf).error.status;
+            if (resGexEm !== null) {
+              await delay(DelaY);
+              const veryf = await functionVerification(resGexEm[0], resGex[1]);
+              // const msg = JSON.parse(veryf).error.status;
+              console.log(veryf);
 
-            if (JSON.parse(veryf).hasOwnProperty("error")) {
-              console.log(
-                "[" +
-                  " " +
-                  moment().format("HH:mm:ss") +
-                  " " +
-                  "]" +
-                  " " +
-                  `Email : ${resGexEm[0]}` +
-                  " " +
-                  "Token Expired"
-              );
-            } else {
-              console.log(
-                "[" +
-                  " " +
-                  moment().format("HH:mm:ss") +
-                  " " +
-                  "]" +
-                  " " +
-                  `Email : ${resGexEm[0]}` +
-                  " " +
-                  "Veryf Sukses"
-              );
+              // if (JSON.parse(veryf).hasOwnProperty("error")) {
+              //   console.log(
+              //     "[" +
+              //       " " +
+              //       moment().format("HH:mm:ss") +
+              //       " " +
+              //       "]" +
+              //       " " +
+              //       `Email : ${resGexEm[0]}` +
+              //       " " +
+              //       "Token Expired"
+              //   );
+              // } else {
+              //   console.log(
+              //     "[" +
+              //       " " +
+              //       moment().format("HH:mm:ss") +
+              //       " " +
+              //       "]" +
+              //       " " +
+              //       `Email : ${resGexEm[0]}` +
+              //       " " +
+              //       "Veryf Sukses"
+              //   );
+              // }
             }
           }
         }
-      } else {
-        console.log(
-          "[" +
-            " " +
-            moment().format("HH:mm:ss") +
-            " " +
-            "]" +
-            " " +
-            `Line Kosong ~`
-        );
       }
     });
   });
